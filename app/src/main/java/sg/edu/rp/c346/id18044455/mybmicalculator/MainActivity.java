4cb939e4477d98629d1efb9e945886c915aa63c3
brandon1804 <18044455@rp.edu.sg>
2020-07-09 12:32:09 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id18044455/mybmicalculator/MainActivity.java
@@ -0,0 +1,123 @@
+package sg.edu.rp.c346.id18044455.mybmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText wT;
+    EditText hT;
+    Button btnC;
+    Button btnRD;
+    TextView tvLCD;
+    TextView tvLCB;
+    TextView tvO;
+    boolean clicked = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wT = findViewById(R.id.etW);
+        wT.requestFocus();
+        hT = findViewById(R.id.etH);
+        btnC = findViewById(R.id.btnC);
+        btnRD = findViewById(R.id.btnRD);
+        tvLCD = findViewById(R.id.tvLCD);
+        tvLCB = findViewById(R.id.tvBMI);
+        tvO = findViewById(R.id.tV0);
+        Calendar now = Calendar.getInstance();  //Create a Calendar object with current date and time
+        final String datetime = now.get(Calendar.DAY_OF_MONTH) + "/" +
+                (now.get(Calendar.MONTH)+1) + "/" +
+                now.get(Calendar.YEAR) + " " +
+                now.get(Calendar.HOUR_OF_DAY) + ":" +
+                now.get(Calendar.MINUTE);
+
+
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double weight = Double.parseDouble(wT.getText().toString());
+                Double height = Double.parseDouble(hT.getText().toString());
+                Double bmiV = weight / (height * height);
+                String bmi = String.format("%.3f",   bmiV);
+                tvLCD.setText(getString(R.string.LCD) + datetime);
+                tvLCB.setText(getString(R.string.LCBMI) + bmi);
+
+                if(bmiV < 18.5){
+                    tvO.setText(R.string.Underweight);
+                }
+                else if(bmiV >= 18.5 && bmiV <= 24.9){
+                    tvO.setText(R.string.Normal);
+                }
+                else if(bmiV >= 25 && bmiV <= 29.9){
+                    tvO.setText(R.string.Overweight);
+                }
+                else{
+                    tvO.setText(R.string.Obese);
+                }
+            }
+        });
+
+        btnRD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wT.setText("");
+                hT.setText("");
+                tvLCD.setText(R.string.LCD);
+                tvLCB.setText(R.string.LCBMI);
+                tvO.setText("");
+                clicked = true;
+            }
+        });
+
+    }//end of main method
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        String pD = tvLCD.getText().toString();
+        String pBMI = tvLCB.getText().toString();
+        SharedPreferences prefsD = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences prefsB = PreferenceManager.getDefaultSharedPreferences(this);
+
+        SharedPreferences.Editor prefsDEdit = prefsD.edit();
+        SharedPreferences.Editor prefsBEdit = prefsB.edit();
+
+        prefsDEdit.putString("date", pD);
+        prefsBEdit.putString("bmi", pBMI);
+
+        prefsDEdit.commit();
+        prefsBEdit.commit();
+
+        if(clicked == true){
+            prefsDEdit.clear();
+            prefsBEdit.clear();
+            prefsDEdit.commit();
+            prefsBEdit.commit();
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences prefsD = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences prefsB = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String pDMSG = prefsD.getString("date", getString(R.string.LCD));
+        String bMSG = prefsB.getString("bmi",  getString(R.string.LCBMI));
+        tvLCD.setText(pDMSG);
+        tvLCB.setText(bMSG);
+    }
+}//end of class
